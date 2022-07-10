3089e58631663cc0896085680c112f4f6c765f1b
Frigiii <frimi999@gmail.com>
2022-07-10 19:48:44 +0200

    Update update.py

--- update.py
+++ update.py
@@ -9,8 +9,20 @@ from unittest import case
 import telepot
 from telepot.loop import MessageLoop
 from apikey import API_KEY
-from subprocess import call
+import subprocess
 
+
+p = subprocess.Popen("/home/frigi/raspberrypi4", shell= True)
+
+p.communicate("sudo systemctl stop bot.service")
+time.sleep(1)
+p.communicate("git pull")
+time.sleep(5)
+p.communicate("sudo systemctl start bot.service")
+p.kill
+
+
+"""
 
 call("sudo systemctl stop bot.service", shell=True)
 time.sleep(1)
@@ -18,3 +30,4 @@ call("cd /home/frigi/raspberrypi4", shell=True)
 call("git pull", shell = True)
 time.sleep(10)
 call("sudo systemctl start bot.service", shell=True)
+"""
