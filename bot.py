f43de5f4759e3054ebe36902574f27b926775de6
Frigiii <frimi999@gmail.com>
2022-07-13 22:18:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,7 +155,7 @@ async def updater(bot: Bot, update: update, update_id) -> None:
 
 async def status(bot: Bot, update: update) -> None:
     if(update.effective_user.username) == "Frigiii":
-        response = subprocess.call('sudo systemctl status bot', stdout = subprocess.PIPE, shell=True)
+        response = subprocess.call('sudo systemctl status bot', stdout = subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
         logger.info("Suceeded with getting status.")
         out = response.communicate()[0]
         #await update.message.reply_text(out)
