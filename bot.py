6684dc8fca0f19367e1f70b691b1ee140c59d901
Frigiii <frimi999@gmail.com>
2022-07-18 22:23:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -169,7 +169,7 @@ async def shell(bot: Bot, update: update, x) -> None:
         x.pop(0)
         if len(x):
             await update.message.reply_text("There we go:")
-            await update.message.reply_text("".join(x))
+            await update.message.reply_text(' '.join(x))
             response = str(subprocess.check_output("".join(x), shell=True))
             await update.message.reply_html(response)
         else:
