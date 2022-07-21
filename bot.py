d1251f3e13b2af4f1e59fd0d711464ef4a558fba
Frigiii <frimi999@gmail.com>
2022-07-21 09:58:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -166,7 +166,11 @@ async def updater(bot: Bot, update: update, update_id) -> None:
         await update.message.reply_text("Sry, but i can't do this for u ;(")
 
 async def shell(bot: Bot, update: update, x) -> None:
-    if(update.effective_user.username) == "rigiii":
+    cont = True
+    for i in x:
+        if i in ['restart','stop']:
+            cont = False
+    if(update.effective_user.username) == "Frigiii" and cont:
         x.pop(0)
         if len(x):
             await update.message.reply_text("There we go:")
