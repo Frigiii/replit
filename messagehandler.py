c060fb0086a4fc9006b79492265ab8e7d43d7a5c
Frigiii <frimi999@gmail.com>
2022-07-27 19:39:04 +0200

    r

--- messagehandler.py
+++ messagehandler.py
@@ -1,3 +1,5 @@
+from itertools import count
+from mimetypes import init
 import time
 from apikey import frigi_channel_id
 from apikey import frigi_chat_id
@@ -5,6 +7,7 @@ import random
 import asyncio
 from typing import NoReturn
 import telegram
+import math
 
 
 async def roll(update, type) -> None:
@@ -52,6 +55,77 @@ async def updater(update, type) -> None:
         text = "Sry, but i can\'t do this for u ;("
     await telegram.request('sendMessage', {'chat_id' : update[type]['chat']['id'], 'text' : text})
 
+async def integrator(update, type) -> None:
+
+    def simpson(f,a,b) -> float:
+        x = a
+        f1 = eval(f)
+        #print(f1)
+        x = b
+        f2 = eval(f)
+        #print(f2)
+        x = (a+b)/2
+        f3 = eval(f)
+        #print(f3)
+        return (f1 + 4*f3 + f2)/6*(b-a)
+    
+
+    def adaptint(a,b,edes,f) -> float:
+        global counter
+        counter += 1
+        i1 = simpson(f,a,b)
+        m = (a+b)/2
+        i2 = simpson(f,a,m) + simpson(f,m,b)
+        e = abs(i2-i1)/15
+        print("Step: {}, Value: {}, accuracy: {}".format(counter,i2,e))
+        if e > edes:
+            return adaptint(a,m,edes/2,f) + adaptint(m,b,edes/2,f)
+        else:
+            return i2
+
+    a = 0
+    b = 10
+    f = "x"
+    e = 1e-6
+    s_max = 1e100
+    all_values = {
+        'f(x)' : f,
+        'a': a,
+        'b': b,
+        'e' : e,
+        'smax' : s_max,
+    }
+    msg = update[type]['text'].split()
+    msg.pop(0)
+    given = {}
+    while len(msg):
+        if msg[0] in all_values:
+            all_values[msg[0]] = format(msg[2])
+            print('found {} = {}'.format(msg[0], all_values[msg[0]]))
+        msg.pop(0)
+    a = float(all_values['a'])
+    b = float(all_values['b'])
+    f = all_values['f(x)']
+    e = float(all_values['e'])
+    s_max = int(all_values['smax'])
+
+    print(a,b,f,e,s_max)
+
+    
+    # if not (a and b and f):
+    #     text = "Integration not correctly initialized. Please enter in following form: f(x) = your_function, a = starting_val, b = end_val."
+    #     await telegram.request('sendMessage', {'chat_id' : update[type]['chat']['id'], 'text' : text})
+    global counter
+    counter = 0
+    text = adaptint(a,b,e,f)
+    #text = simpson(f,a,b)
+    #print(text)
+    text = "Got the result : {}".format(text)
+    await telegram.request('sendMessage', {'chat_id' : update[type]['chat']['id'], 'text' : text})
+
+
+        
+
 
 functions = {
     '/roll' : roll,
@@ -60,6 +134,7 @@ functions = {
     '/hello': hello,
     '/myinfo': myinfo,
     '/updater': updater,
+    '/integrate': integrator,
 }
 
 async def handler(update) -> str:
